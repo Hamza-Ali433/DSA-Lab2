7622e67431d8f97b182b655e1def664b74dc4934
Muhammad Hamza Ali <hamzajatt177@gmail.com>
2026-09-18 16:02:25 +0500

    Implement isSorted function

--- Sorted_Array.cpp
+++ Sorted_Array.cpp
@@ -61,6 +61,21 @@ void empty(){
 }
 
 // Sorted Function implementation 
+bool isSorted(const int* arr, const int size) {
+    if(size<=1){return false;
+    }
+
+    
+    for (int i = 0; i < size - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false; 
+            cout<<"False"<<endl;
+        }
+    }
+    return true;
+    cout<<"True"<<endl;
+}
+
 
 
 
